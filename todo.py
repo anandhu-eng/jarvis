2794b227d81306ef63d7ac4df95bafc1232da68e
anandhu-eng <anandhukicks@gmail.com>
2021-12-26 22:25:47 +0530

    modified

--- todo.py
+++ todo.py
@@ -4,3 +4,17 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 
 print("Opened database successfully")
+option=int(input("1 for adding task, 2 for viewing all the task:"))
+if(option==1):
+    task=input("Enter the task:")
+    inst="INSERT INTO tasks (Task) VALUES ('{task}');".format(task=task)
+    conn.execute("INSERT INTO tasks (Task) VALUES ('{task}');".format(task=task))
+    conn.commit()
+    print("task successfully added!",inst)
+else:
+    cursor = conn.execute("SELECT Number, Task from tasks")
+    for i in cursor:
+        print(i[1])
+
+
+conn.close()
